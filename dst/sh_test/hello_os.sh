3dd83bc2ffc2f341742e678d118b6290327b5f0e
杨荣津 <21375077@buaa.edu.cn>
2024-03-12 20:22:35 +0800

    加了引号

--- dst/sh_test/hello_os.sh
+++ dst/sh_test/hello_os.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-touch $2
-awk 'NR=8 || NR==32 || NR==128 || NR==512 || NR==1024 {print}' $1 > $2
+touch "$2"
+awk 'NR=8 || NR==32 || NR==128 || NR==512 || NR==1024 {print}' "$1" > "$2"
+
