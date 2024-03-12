d16cfe720a1ed3d17acd684513fb4f928fa55c77
杨荣津 <21375077@buaa.edu.cn>
2024-03-12 19:54:30 +0800

    切换到mac

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -2,10 +2,4 @@
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
 
-# 1. 获取源文件 需要查找的字符串 新生成的文件
-srcFile="$1"
-targetStr="$2"
-newFile="$3"
-
-# 2. 使用grep查找内容
-grep -n "$targetStr" "$srcFile" | awk -F: '{print $1}'  > "$newFile"
+grep -n $2 $1 | awk -F: '{print $1}'  > $3
