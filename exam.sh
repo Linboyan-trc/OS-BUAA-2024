57d3dd4f4a6745ee0aaef681716d445c610041e0
杨荣津 <21375077@buaa.edu.cn>
2024-03-13 19:38:58 +0800

    cundang exam.sh

--- exam.sh
+++ exam.sh
@@ -4,12 +4,22 @@ cat ./code/14.c
 
 cd test/code
 
-i=0
-while (($i < 16 ))
-do
-	gcc -o $i.o $i.c
-	let i=i+1
-done
+gcc -c 0.c
+gcc -c 1.c
+gcc -c 2.c
+gcc -c 3.c
+gcc -c 4.c
+gcc -c 5.c
+gcc -c 6.c
+gcc -c 7.c
+gcc -c 8.c
+gcc -c 9.c
+gcc -c 10.c
+gcc -c 11.c
+gcc -c 12.c
+gcc -c 13.c
+gcc -c 14.c
+gcc -c 15.c
 
 cd ..
 gcc -o hello code/*.o
@@ -21,3 +31,17 @@ mv err.txt ../
 
 cd ..
 chmod 655 err.txt
+
+case $# in
+0)
+	awk 'NR==2 {print}' err.txt >&2
+;;
+1)
+	temp1=$(($1+1))
+	awk "NR==$temp1 {print}" err.txt >&2
+;;
+2)
+	temp2=$(($1+$2))
+	awk "NR==$temp2 {print}" err.txt >&2
+;;
+esac
