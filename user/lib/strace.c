d4e2f119b51b7c04d227494cc821505263ee1e6d
杨荣津 <21375077@buaa.edu.cn>
2024-05-22 21:16:58 +0800

    2

--- user/lib/strace.c
+++ user/lib/strace.c
@@ -18,38 +18,38 @@ void strace_send(int sysno) {
 		return;
 	}
 
-	// Your code here. (1/2)
+	// Your code here. (1/2
+        // Your code here. (1/2)
          if (straced == 0) {
-	 	//debugf("strace test passed!\n");   
-	 	 return;
+                return;
          } else {
-		//debugf("strace test passed!\n");
-		 int old_straced = straced;
-	    	straced = 0;
-	    	
-	    	ipc_send(env->env_parent_id, sysno, NULL, 0);
-	    	
-	    	syscall_set_env_status(env->env_id, ENV_NOT_RUNNABLE);
-	    	
-	    	straced = old_straced;
+                int old_straced = straced;
+                straced = 0;
+            
+                ipc_send(env->env_parent_id, sysno, 0, 0);
+            
+                syscall_set_env_status(env->env_id, ENV_NOT_RUNNABLE);
+            
+                straced = old_straced;
          }
 }
 
 void strace_recv() {
-	// Your code here. (2/2)
-	 while(1) {
-        	int sysno = 0;
+        // Your code here. (2/2)
+        while(1) {
+                int sysno = 0;
         
-        	sysno = ipc_recv((void *)env->env_ipc_from, (void *)env->env_ipc_dstva, (void *)env->env_ipc_perm);
-		
-			strace_barrier(env->env_ipc_from);
-			
-		recv_sysno(env->env_ipc_from, sysno);
-			
-		syscall_set_env_status(env->env_ipc_from, ENV_RUNNABLE);
-			
-		if (sysno == SYS_env_destroy) {
-		return;
-		}
-	}
+                int whom = 0;
+                sysno = ipc_recv(&whom, 0, 0);
+
+                strace_barrier(whom);
+
+                recv_sysno(whom, sysno);
+
+                syscall_set_env_status(whom, ENV_RUNNABLE);
+
+                if (sysno == SYS_env_destroy) {
+                        return;
+                }
+        }
 }
