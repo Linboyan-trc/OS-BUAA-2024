b77f76fa9a1a29932a0d64af25be1331a1928348
杨荣津 <21375077@buaa.edu.cn>
2024-06-16 21:14:24 +0800

    rm file

--- user/rm.c
+++ user/rm.c
@@ -1,5 +1,15 @@
 #include <lib.h>
 
 int main(int argc, char **argv) {
+	if (argc != 2) {
+		debugf("rm:缺少文件名\n");
+	}
+
+	int r = remove(argv[1]);
+	if (r < 0) {
+		printf("rm: cannot remove \'%s\': No such file or directory",argv[1]);
+		return -1;
+	}
+
 	return 0;
 }
