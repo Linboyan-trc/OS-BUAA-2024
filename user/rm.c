9d72204305d95a01b28c4aead266fce56a151a98
杨荣津 <21375077@buaa.edu.cn>
2024-06-16 21:18:15 +0800

    rm refuse dir

--- user/rm.c
+++ user/rm.c
@@ -5,9 +5,14 @@ int main(int argc, char **argv) {
 		debugf("rm:缺少文件名\n");
 	}
 
+	struct Stat stat_buf;
+	if (stat(argv[1], &stat_buf) >= 0) {
+		printf("rm: cannot remove \'%s\': Is a directory\n", argv[1]);
+	}
+
 	int r = remove(argv[1]);
 	if (r < 0) {
-		printf("rm: cannot remove \'%s\': No such file or directory",argv[1]);
+		printf("rm: cannot remove \'%s\': No such file or directory\n",argv[1]);
 		return -1;
 	}
 
